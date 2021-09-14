cb5a790eda1878710c16c40611dad2cbc0d323ee
Najib Mozahem <najib.mozahem@gmail.com>
2021-09-14 20:00:16 +0300

    started clustering

--- scripts/working_script.R
+++ scripts/working_script.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(cluster)
+library(Rtsne)
 
 the_data <- read_tsv("dataset/marketing_campaign.csv")
 
@@ -20,6 +22,11 @@ ggplot(the_data) + geom_boxplot(aes(Income))
 ## all other values
 
 the_data <- the_data[the_data$Income < 666666, ]
+
+## we also note that the two variables Z_costcontact and Z_revenue do not
+## change. We therefore remove them since they add no information:
+
+the_data <- the_data[, -which(names(the_data) %in% c("Z_CostContact", "Z_Revenue"))]
 
 ## look at the structure
 str(the_data)
@@ -79,6 +86,39 @@ the_data %>%
                       fill = "blue") 
 
 ggplot(the_data, aes(Income, pct_wine)) + geom_point() + 
-  geom_smooth()
+  geom_smooth() + scale_x_log10()
 
+## cluster analysis
+the_data <- the_data %>% select(Year_Birth, Education, Marital_Status,
+                                Income, Kidhome, Teenhome, Recency,
+                                NumDealsPurchases, NumWebPurchases, NumCatalogPurchases,
+                                NumStorePurchases, NumWebVisitsMonth, AcceptedCmp3,
+                                AcceptedCmp4, AcceptedCmp5, AcceptedCmp1, AcceptedCmp2,
+                                Complain, Response, total_spent, pct_wine, pct_fruit,
+                                pct_meat, pct_sweets, pct_gold)
 
+gower_df <- daisy(the_data, metric = "gower", type = list(logratio = 4))
+summary(gower_df)
+
+silhouette <- c()
+silhouette <- c(silhouette, NA)
+for(i in 2:10) {
+  pam_clusters = pam(as.matrix(gower_df), diss = TRUE, k = i)
+  silhouette = c(silhouette, pam_clusters$silinfo$avg.width)
+}
+cluster_data <- data.frame(Clusters = 1:10, Silhouette_width = silhouette)
+ggplot(cluster_data, aes(Clusters, Silhouette_width)) + geom_point() + 
+  geom_line() + scale_x_continuous(breaks = c(1:10))
+
+pam_data = pam(gower_df, diss = TRUE, k = 3)
+the_data[pam_data$medoids, ]
+the_data <- the_data %>% mutate(cluster = pam_data$clustering)
+
+ggplot(the_data) + geom_point(aes(total_spent, Income, 
+                                  color = factor(cluster)),
+                              alpha = 0.6)
+
+tsne_object <- Rtsne(gower_df, is_distance = TRUE)
+tsne_df <- tsne_object$Y %>% data.frame() %>% setNames(c("X", "Y")) %>% 
+  mutate(cluster = factor(pam_data$clustering))
+ggplot(tsne_df, aes(X, Y)) + geom_point(aes(color = cluster))
